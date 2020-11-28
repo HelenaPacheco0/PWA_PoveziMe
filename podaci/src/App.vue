a9686dad10c840fecc651ea5ac1238fe2e49e63a
MariaHelenaDakic <maria.helena.dakic@users.noreply.github.com>
2020-11-28 23:54:01 +0100

    init

--- podaci/src/App.vue
+++ podaci/src/App.vue
@@ -1,16 +1,26 @@
 <template>
   <div id="app">
-    <UnosPodataka/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+      <router-view/>
   </div>
 </template>
 
 <script>
 import UnosPodataka from './components/UnosPodataka.vue'
 
+
+
 export default {
   name: 'App',
   components: {
     UnosPodataka
+  },
+  name: 'Home',
+  components: {
+    Home
   }
 }
 </script>
---
+++ podaci/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <b-table :data="data" :columns="columns"></b-table>
+    <UnosPodataka />
+    <Home />
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+    Home
+  },
+}
+</script>
